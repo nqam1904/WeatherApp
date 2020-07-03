e32c607c3874c4cfec234799c51e0b0f2698840e
Minh <nghiemminh1904@gmail.com>
2020-07-03 14:00:50 +0700

    luu lich su khi search thanh pho khac

--- src/App.js
+++ src/App.js
@@ -17,59 +17,90 @@ class App extends Component {
       weather_descriptions: '',
       isShowCard: false,
       isLoading: false,
-      isQuery: ''
+      history: !localStorage.getItem(`weatherApp`) ? [] :
+        JSON.parse(localStorage.getItem(`weaterApp`)),
     }
 
+  }
+
+  functionIf = (name, res) => {
+    if (!name) {
+      this.setState({
+        isLoading: !this.state.isLoading,
+      });
+    } else {
+      this.setState({
+        name: res.location.name,
+        temperature: res.current.temperature,
+        humidity: res.current.humidity,
+        weather_descriptions: res.current.weather_descriptions,
+      })
+    }
   }
 
   getWeather = async (e) => {
     e.preventDefault();
     const name = e.target.elements.name.value;
     e.target.reset(); // xóa chuỗi trong input
-    let data = [];
+    let history = JSON.parse(localStorage.getItem(`weatherApp`));
     if (name === "") {
       toast.warn('Vui lòng nhập tên thành phố !');
     } else {
-
-      const api_call = await fetch(`http://api.weatherstack.com/current?access_key=8a35c848dd7c818c2b72ebf4b80837eb&query=${name}`);
-
-      const res = await api_call.json();// trả dữ liệu api về
-      data[0] = res;
-      localStorage.setItem("res", JSON.stringify(data));
-      let list = JSON.parse(localStorage.getItem("res"));
-      list.push(res);
-      localStorage.setItem("res", JSON.stringify(list));
-      console.log(res);
-      if (!name) {
-        return toast.error("Không tìm thấy dữ liệu!");
-
-
-      }
       this.setState({
-        name: res.location.name,
-        temperature: res.current.temperature,
-        humidity: res.current.humidity,
-        weather_descriptions: res.current.weather_descriptions,
-        isShowCard: !this.state.isShowCard, // hiển thị thông tin thành phố
-
+        isLoading: !this.state.isLoading,
+        isShowCard: false,
       });
+      try {
+        const api_call = await fetch(`http://api.weatherstack.com/current?access_key=8a35c848dd7c818c2b72ebf4b80837eb&query=${name}`);
+        const res = await api_call.json();// trả dữ liệu api về
+        if (res.success === undefined) {
+          localStorage.setItem(`weatherApp`, JSON.stringify([res]));
+          this.functionIf(name, res);
+          this.setState((state) => {
+            state.history.push(res);
+            state.isShowCard = true;
+          });
+        } else {
+          await history.push(res);
+          localStorage.setItem(`weatherApp`, JSON.stringify(history));
+          this.functionIf(name, res);
+          this.setState((state) => {
+            state.history.push(res);
+            state.isShowCard = true;
+          });
+        }else {
+          toast.error(`Không tìm thấy thành phố này`);
+        }
 
+
+      } finally {
+        this.setState({
+          isLoading: !this.state.isLoading,
+        });
+      }
     }
-  }
-  getData = () => {
-
-  }
+  };
   render() {
-    const { isShowCard, isLoading, isQuery } = this.state;
-    const elementsCard = isShowCard ? (<CardResult name={this.state.name}
-      temperature={this.state.temperature}
-      humidity={this.state.humidity}
-      weather_descriptions={this.state.weather_descriptions} />) : ''; // ẩn component card khi chưa tìm thành phố
-    const result = isLoading ? <Spinner size={32} speed={1} animating={true} /> : (elementsCard);
+    const { isShowCard, isLoading, history } = this.state;
+    const elementsCard = isShowCard ? (
+      <CardResult
+        name={this.state.name}
+        temperature={this.state.temperature}
+        humidity={this.state.humidity}
+        weather_descriptions={this.state.weather_descriptions}
+      />
+    ) : (
+        ""
+      ); // ẩn component card khi chưa tìm thành phố
+    const result = isLoading ? (
+      <Spinner size={32} speed={1} animating={true} />
+    ) : (
+        elementsCard
+      );
     return (
       <Container>
         <div className="form">
-          <FormSearch loadweather={this.getWeather} value={isQuery} />
+          <FormSearch loadweather={this.getWeather} />
           <ToastContainer autoClose={3000} />
         </div>
         <Row>
@@ -79,7 +110,21 @@ class App extends Component {
           </Col>
           <Col sm={6}>
             <p>Lịch sử</p>
-
+            {history.length === 0 ? (
+              ''
+            ) : (
+                <div>
+                  {history.map((i, index) => (
+                    <CardResult
+                      key={index}
+                      name={i.location.name}
+                      temperature={i.current.temperature}
+                      humidity={i.current.humidity}
+                      weather_descriptions={i.current.weather_descriptions}
+                    />
+                  ))}
+                </div>
+              )}
           </Col>
         </Row>
       </Container>
